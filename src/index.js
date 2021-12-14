ced5ef73de186695c1a5106ca5a529a63ffff1c5
vivek-hash <vivekcs011@gmail.com>
2021-12-14 14:44:27 +0530

    final-commit

--- src/index.js
+++ src/index.js
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Bmi from './Bmi';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Bmi/>
   </React.StrictMode>,
   document.getElementById('root')
 );
